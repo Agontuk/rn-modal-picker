b760208a398ec0548cb6dd2ea65561da816207b3
Iftekhar Rifat <rifat662@gmail.com>
2017-08-09 20:01:49 +0600

    Add eslint rules & fix errors

--- src/PickerAndroid.js
+++ src/PickerAndroid.js
@@ -3,45 +3,46 @@ import PropTypes from 'prop-types';
 import { Dimensions, Modal, StyleSheet, View } from 'react-native';
 
 const Props = {
-  title: PropTypes.string.isRequired,
-  visible: PropTypes.bool.isRequired,
-  onCancel: PropTypes.func.isRequired
+    title: PropTypes.string.isRequired,
+    visible: PropTypes.bool.isRequired,
+    onCancel: PropTypes.func.isRequired
 };
 
+// eslint-disable-next-line react/prefer-stateless-function
 class PickerAndroid extends Component {
-  static propTypes = Props;
+    static propTypes = Props;
 
-  render() {
-    const { title, visible, onCancel } = this.props;
+    render() {
+        // eslint-disable-next-line no-unused-vars
+        const { title, visible, onCancel } = this.props;
 
-    return (
-      <Modal
-        animationType='fade'
-        transparent={false}
-        visible={visible}
-        onRequestClose={onCancel}
-      >
-        <View style={styles.container}>
-          <View style={styles.innerContainer}>
-          </View>
-        </View>
-      </Modal>
-    );
-  }
+        return (
+            <Modal
+                animationType='fade'
+                transparent={false}
+                visible={visible}
+                onRequestClose={onCancel}
+            >
+                <View style={styles.container}>
+                    <View style={styles.innerContainer} />
+                </View>
+            </Modal>
+        );
+    }
 }
 
 export default PickerAndroid;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  innerContainer: {
-    backgroundColor: 'white',
-    width: Dimensions.get('window').width * 0.75,
-    height: Dimensions.get('window').height * 0.75
-  }
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    innerContainer: {
+        backgroundColor: 'white',
+        width: Dimensions.get('window').width * 0.75,
+        height: Dimensions.get('window').height * 0.75
+    }
 });
--- src/PickerIOS.js
+++ src/PickerIOS.js
@@ -2,100 +2,102 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-// iOS blue from https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
+// IOS blue from https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
 const IOS_BLUE = '#007AFF';
 const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
 
 const Props = {
-  title: PropTypes.string.isRequired,
-  data: PropTypes.arrayOf(
-    PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.string
-    ])
-  ).isRequired,
-  visible: PropTypes.bool.isRequired,
-  onCancel: PropTypes.func.isRequired
+    title: PropTypes.string.isRequired,
+    // eslint-disable-next-line react/no-unused-prop-types
+    data: PropTypes.arrayOf(
+        PropTypes.oneOfType([
+            PropTypes.object,
+            PropTypes.string
+        ])
+    ).isRequired,
+    visible: PropTypes.bool.isRequired,
+    onCancel: PropTypes.func.isRequired
 };
 
+// eslint-disable-next-line react/prefer-stateless-function
 class PickerIOS extends Component {
-  static propTypes = Props;
+    static propTypes = Props;
 
-  render() {
-    const { title, visible, onCancel } = this.props;
+    render() {
+        const { title, visible, onCancel } = this.props;
 
-    return (
-      <Modal
-        animationType='slide'
-        transparent={false}
-        visible={visible}
-        onRequestClose={onCancel}
-      >
-        <View style={styles.container}>
-          <View style={styles.innerContainer}>
-            <TouchableOpacity style={styles.cancelContainer} onPress={onCancel}>
-              <Text style={styles.cancel}>Cancel</Text>
-            </TouchableOpacity>
+        return (
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={visible}
+                onRequestClose={onCancel}
+            >
+                <View style={styles.container}>
+                    <View style={styles.innerContainer}>
+                        <TouchableOpacity style={styles.cancelContainer} onPress={onCancel}>
+                            <Text style={styles.cancel}>Cancel</Text>
+                        </TouchableOpacity>
 
-            <View style={styles.titleContainer}>
-              <Text style={styles.title}>{title}</Text>
-            </View>
+                        <View style={styles.titleContainer}>
+                            <Text style={styles.title}>{title}</Text>
+                        </View>
 
-            {false &&
-            <TouchableOpacity style={styles.doneContainer} onPress={onCancel}>
-              <Text style={styles.done}>Done</Text>
-            </TouchableOpacity>
-            }
-          </View>
-        </View>
-      </Modal>
-    );
-  }
+                        {false &&
+                        <TouchableOpacity style={styles.doneContainer} onPress={onCancel}>
+                            <Text style={styles.done}>Done</Text>
+                        </TouchableOpacity>
+                        }
+                    </View>
+                </View>
+            </Modal>
+        );
+    }
 }
 
 export default PickerIOS;
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: STATUSBAR_HEIGHT,
-    backgroundColor: '#F7F7F7',
-    height: STATUSBAR_HEIGHT + APPBAR_HEIGHT,
-    shadowColor: 'black',
-    shadowOpacity: 0.1,
-    shadowRadius: StyleSheet.hairlineWidth,
-    shadowOffset: {
-      height: StyleSheet.hairlineWidth
+    container: {
+        paddingTop: STATUSBAR_HEIGHT,
+        backgroundColor: '#F7F7F7',
+        height: STATUSBAR_HEIGHT + APPBAR_HEIGHT,
+        shadowColor: 'black',
+        shadowOpacity: 0.1,
+        shadowRadius: StyleSheet.hairlineWidth,
+        shadowOffset: {
+            height: StyleSheet.hairlineWidth
+        }
+    },
+    innerContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titleContainer: {
+        flex: 1,
+        position: 'absolute'
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: '600'
+    },
+    cancelContainer: {
+        position: 'absolute',
+        left: 16
+    },
+    doneContainer: {
+        position: 'absolute',
+        right: 16
+    },
+    cancel: {
+        color: IOS_BLUE,
+        fontSize: 18
+    },
+    done: {
+        color: IOS_BLUE,
+        fontSize: 18
     }
-  },
-  innerContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  titleContainer: {
-    flex: 1,
-    position: 'absolute'
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600'
-  },
-  cancelContainer: {
-    position: 'absolute',
-    left: 16
-  },
-  doneContainer: {
-    position: 'absolute',
-    right: 16
-  },
-  cancel: {
-    color: IOS_BLUE,
-    fontSize: 18
-  },
-  done: {
-    color: IOS_BLUE,
-    fontSize: 18
-  }
 });
