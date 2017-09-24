57b710faa1337f4741ba9049d9e7974409384d67
Iftekhar Rifat <rifat662@gmail.com>
2017-09-24 16:00:46 +0600

    Update PickerIOS style

--- src/PickerIOS.js
+++ src/PickerIOS.js
@@ -14,7 +14,7 @@ import { Props } from './Utils';
 // IOS blue from https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
 const IOS_BLUE = '#007AFF';
 const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
+const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
 
 // eslint-disable-next-line react/prefer-stateless-function
 class PickerIOS extends Component {
@@ -58,15 +58,25 @@ export default PickerIOS;
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: STATUSBAR_HEIGHT,
-        backgroundColor: '#F7F7F7',
-        height: STATUSBAR_HEIGHT + APPBAR_HEIGHT,
-        shadowColor: 'black',
-        shadowOpacity: 0.1,
-        shadowRadius: StyleSheet.hairlineWidth,
-        shadowOffset: {
-            height: StyleSheet.hairlineWidth
-        }
+        paddingTop: STATUS_BAR_HEIGHT,
+        height: STATUS_BAR_HEIGHT + APPBAR_HEIGHT,
+        ...Platform.select({
+            ios: {
+                backgroundColor: '#FFF',
+                shadowColor: 'black',
+                shadowOpacity: 0.1,
+                shadowRadius: StyleSheet.hairlineWidth,
+                shadowOffset: {
+                    height: StyleSheet.hairlineWidth
+                },
+                borderBottomWidth: StyleSheet.hairlineWidth,
+                borderBottomColor: 'rgba(0, 0, 0, .3)'
+            },
+            android: {
+                backgroundColor: '#F7F7F7',
+                elevation: 4
+            }
+        })
     },
     innerContainer: {
         flex: 1,
